98983b0dd5a5c94f76571e77ccf0f4be9b8ba8e2
longmain <longmain@longmain.cn>
2019-07-01 21:10:05 +0800

    add packages

---
+++ stm32l151/7_M5310A_UDP/build/keil/Obj/context_rvds.d
@@ -0,0 +1,1 @@
+.\build\keil\obj\context_rvds.o: ..\..\rt-thread\libcpu\arm\cortex-m3\context_rvds.S
---
+++ stm32l151/7_M5310A_UDP/build/keil/Obj/startup_stm32l151xc.d
@@ -0,0 +1,1 @@
+.\build\keil\obj\startup_stm32l151xc.o: ..\..\libraries\STM32L1xx_HAL\CMSIS\Device\ST\STM32L1xx\Source\Templates\arm\startup_stm32l151xc.s
